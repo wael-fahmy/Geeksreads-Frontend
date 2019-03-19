db5f19a02d2b4ec4236f68d9cd3b47d374076c1b
Kanda <m.mufeed1996@gmail.com>
2019-03-20 00:13:14 +0200

    added main book entity

--- src/app/book-entity/book-entity.component.ts
+++ src/app/book-entity/book-entity.component.ts
@@ -11,5 +11,19 @@ export class BookEntityComponent implements OnInit {
 
   ngOnInit() {
   }
+  more_book_discription() {
+    const dots = document.getElementById('dots_book_discription');
+    const moreText = document.getElementById('more_book_discription');
+    const btnText = document.getElementById('myBtn_book_discription');
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Read more';
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = 'Read less';
+      moreText.style.display = 'inline';
+    }
+  }
 
 }
