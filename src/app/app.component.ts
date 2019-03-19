9d4fd217918dd9498c806e24de27669dbb0db8e3
Ahmed M. Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-03-19 15:50:30 +0200

    Adjusted some files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Router, RouterEvent, NavigationEnd } from "@angular/router";
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
