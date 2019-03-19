dfe38551fd172ab95ce75bb968e0175cb58d0741
Ahmed M. Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-03-19 14:07:03 +0200

    Added homepage component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MaterialModule } from './material.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
+    HomepageComponent,
   ],
   imports: [
     BrowserModule,
