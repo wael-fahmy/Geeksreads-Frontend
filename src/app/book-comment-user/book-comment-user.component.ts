204e12a7fdd2313e3479f5f5b0f1139479734a79
Kanda <m.mufeed1996@gmail.com>
2019-03-20 23:40:40 +0200

    added user review static

--- src/app/book-comment-user/book-comment-user.component.ts
+++ src/app/book-comment-user/book-comment-user.component.ts
@@ -8,8 +8,38 @@ import { Component, OnInit } from '@angular/core';
 export class BookCommentUserComponent implements OnInit {
 
   constructor() { }
-
-  ngOnInit() {
+like() {
+  let likes = 60;
+  // tslint:disable-next-line:variable-name
+  let user_liked = 0;
+  document.getElementById('show_likes').innerHTML = likes.toString();
+  if (user_liked === 0) {
+              likes = likes + 1;
+              user_liked = 1;
+          } else {
+        if ( likes === 0) {
+          likes = 0;
+        } else {
+          likes = likes - 1;
+          user_liked = 0;
+        }
+      }
   }
+more_user_preview() {
+    const dots = document.getElementById('dots_user_review');
+    const moreText = document.getElementById('more_review');
+    const btnText = document.getElementById('myBtn_user_review');
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Read more';
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = 'Read less';
+      moreText.style.display = 'inline';
+    }
+  }
+ngOnInit() {
+    }
 
 }
