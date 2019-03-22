754c1026aa68e8b36575faf9177958f7c33bf39c
Kanda <m.mufeed1996@gmail.com>
2019-03-22 19:22:58 +0200

    added json for book info part

--- backend_service/app.js
+++ backend_service/app.js
@@ -26,7 +26,36 @@ app.use((req, res, next) => {
     message: 'Post added successfully'
   });
 });*/
-
+app.use("/api/book",(req,res,next)=> {
+  const book_details=[
+    {
+    book_title:'Hajimete no Gal',
+    book_image:'https://upload.wikimedia.org/wikipedia/en/thumb/5/52/Hajimeta_no_Gal%2C_Volume_1.jpg/220px-Hajimeta_no_Gal%2C_Volume_1.jpg',
+    book_author:'Sebastian',
+    book_details_shown:'Spring. The "season of love" has arrived and it seems that finding himself a girlfriend was harder than Junichi believed. To break the status quo,',
+    book_details_hidden:'Junichis friends have forced him into confessing to the gal, Yame Yukana. However, things do not go quite as he expected. A series of "firsts" begins!'
+    },
+    {
+    book_title:'Black Butler',
+    book_image:'https://i.pinimg.com/236x/d6/b0/7b/d6b07b9d0ee3df0168945910e2142bf5--black-butler-kuroshitsuji-shinigami.jpg',
+    book_author:'WiessMan',
+    book_details_shown:'Black Butler (Japanese: 黒執事 Hepburn: Kuroshitsuji) is a Japanese manga series written and illustrated by Yana Toboso. Since its debut on September 16, 2006,',
+    book_details_hidden:' it has been serialized in Gangan Comics sshōnen manga magazine Monthly GFantasy. ... A live action film adaptation was released in Japan on January 18, 2014'
+    },
+    {
+    book_title:'Gal Gohan',
+    book_image:'https://pbs.twimg.com/media/DenR6slUcAA31YT.jpg',
+    book_author:'Loyd',
+    book_details_shown:'Home Economics Teacher, Shinji Yabe, is asked by the biggest gyaru in school, Miku Okazaki, to help her "bake cookies to give to the teachers". The principal told her to do this, after she flunked every subject and makeup-class they put her in. He gets her to successfully finish a batch of cookies after several mishaps and is touched by his students glee over her success.',
+    book_details_hidden:'He decides to start a cooking club after this experience to get more time and connection with his students. Upon hearing this, Miku decides to join, since he was the first teacher that didnt shove her into makeup-lessons after her failings and pushed her to try harder instead.'
+    }
+  ];
+    
+  res.status(200).json({
+    message: "User fetched successfully!",
+    book_details: book_details
+    });
+  });
 app.use("/api/list", (req, res, next) => {
   const Books = [
     {
