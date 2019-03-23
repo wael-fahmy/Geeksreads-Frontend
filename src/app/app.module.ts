b8cc14e7374867bb887d019ae6c2c37eb50947f5
Ahmed Mohamed Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-03-23 17:40:50 +0200

    Redesigned the Sign In page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MAT_LABEL_GLOBAL_OPTIONS } from '@angular/material';
 import { FooterComponent } from './footer/footer.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -61,13 +62,11 @@ import { ProfileEditComponent } from './profile-edit/profile-edit.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
-  providers: [
-    {
-      provide: MAT_LABEL_GLOBAL_OPTIONS, useValue: {float: 'never'}
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
