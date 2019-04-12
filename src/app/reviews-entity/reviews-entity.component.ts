40b4559ccf0afad8c5d26dd2b3c8a5977c106f37
Kanda <m.mufeed1996@gmail.com>
2019-04-12 18:42:35 +0200

    added reviews entity static

--- src/app/reviews-entity/reviews-entity.component.ts
+++ src/app/reviews-entity/reviews-entity.component.ts
@@ -11,5 +11,18 @@ export class ReviewsEntityComponent implements OnInit {
 
   ngOnInit() {
   }
-
+  more_review_discription() {
+    const dots = document.getElementById('dots-user-review');
+    const moreText = document.getElementById('more-review');
+    const btnText = document.getElementById('myBtn-user-review');
+    if (dots.style.display === 'none') {
+      dots.style.display = 'inline';
+      btnText.innerHTML = 'Read Full Review';
+      moreText.style.display = 'none';
+    } else {
+      dots.style.display = 'none';
+      btnText.innerHTML = 'Show Less Review';
+      moreText.style.display = 'inline';
+    }
+  }
 }
