d3d95cf39046b11d73be1550b2459a63747800a2
Ahmed Mohamed Salah <ahmed.mohamed973@eng-st.cu.edu.eg>
2019-04-15 23:44:07 +0200

    Some signout modifications

--- src/app/sign-out/sign-out.component.ts
+++ src/app/sign-out/sign-out.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
 
+/**
+ *  Signout Component
+ *  @export
+ *  @class SignOutComponent
+ *  @implements {OnInit}
+ */
 @Component({
   selector: 'app-sign-out',
   templateUrl: './sign-out.component.html',
   styleUrls: ['./sign-out.component.css']
 })
 export class SignOutComponent implements OnInit {
+
+  /**
+   *  Creates an instance of SignOutComponent.
+   *  @memberof SignOutComponent
+   */
   constructor() { }
 
-  ngOnInit() {
-  }
+  /**
+   *  Angular ngOnInit
+   * @memberof SignOutComponent
+   */
+  ngOnInit() { }
 
 }
