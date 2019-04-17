7c7c43dea6baba619bfd89e8210a59d90c999c65
Ayahossam95 <ayahossam_95@hotmail.com>
2019-04-17 07:28:43 +0200

    Testing sign in and up and newsfeed done

--- e2e/src/signin/login.e2e-spec.ts
+++ e2e/src/signin/login.e2e-spec.ts
@@ -3,6 +3,8 @@ import { browser, logging } from 'protractor';
 import { protractor } from 'protractor/built/ptor';
 import { testCases as emailTestCases } from './testCases-signin-email';
 import { testCases as pwdTestCases } from './testCases-signin-pwd';
+import { testCases as nameTestCases } from './testCases-signin-name';
+import { testCases as allTestCases } from './testCases-signin-withall';
 
 // import * as fooo from 'testCases-signin-email.json';
 
@@ -25,51 +27,89 @@ describe('Login tests', () => {
 
 
     /******************************************************************************************************* */
-    // EMAIL
+    // ALL at ONCE: user name, email and password
+    // allTestCases.valid.forEach((testCase) => {
+    it('Sign-in form with EMAIL and PAsswrod : ' + '"aya.karmo95@gmail.com" and "a1@aya"' + '   SHOULD BE : VALID', () => {
 
-    emailTestCases.valid.forEach((testCase) => {
-        it('Signin form: EMAIL INPUT : ' + testCase + '   SHOULD BE : VALID', () => {
-            emailBox.clear().then(() => {
-                emailBox.sendKeys(testCase).then(() => {
-                    expect(emailBox.getAttribute('class')).toContain('ng-valid');
-                });
+        emailBox.clear().then(() => {
+            emailBox.sendKeys('aya.karmo95@gmail.com').then(() => {
+                expect(emailBox.getAttribute('class')).toContain('ng-valid');
             });
-        });
-    });
 
-    emailTestCases.invalid.forEach((testCase) => {
-        it('Signin form: EMAIL INPUT : ' + testCase + '   SHOULD BE : INVALID', () => {
-            emailBox.clear().then(() => {
-                emailBox.sendKeys(testCase).then(() => {
-                    expect(emailBox.getAttribute('class')).toContain('ng-invalid');
-                });
-            });
-        });
-    });
-
-    /******************************************************************************************************* */
-    // PASSWORD
-
-    pwdTestCases.valid.forEach((testCase) => {
-        it('Signin form: PASSWORD INPUT : ' + testCase + '   SHOULD BE : VALID', () => {
             pwdBox.clear().then(() => {
-                pwdBox.sendKeys(testCase).then(() => {
+                pwdBox.sendKeys('a1@aya').then(() => {
                     expect(pwdBox.getAttribute('class')).toContain('ng-valid');
                 });
             });
         });
     });
 
+    /******************************************************************************************************* */
+    // ALL at ONCE: user name, email and password
+    // allTestCases.valid.forEach((testCase) => {
+    it('Sign-in form with EMAIL and PAsswrod : ' + '"ayahossam_95@hotmail.com" and "tmam123"' + '   SHOULD BE : VALID', () => {
 
-    pwdTestCases.invalid.forEach((testCase) => {
-        it('Signin form: PASSWORD INPUT : ' + testCase + '   SHOULD BE : INVALID', () => {
+        emailBox.clear().then(() => {
+            emailBox.sendKeys('ayahossam_95@hotmail.com').then(() => {
+                expect(emailBox.getAttribute('class')).toContain('ng-valid');
+            });
+
             pwdBox.clear().then(() => {
-                pwdBox.sendKeys(testCase).then(() => {
-                    expect(pwdBox.getAttribute('class')).toContain('ng-invalid');
+                pwdBox.sendKeys('tmam123').then(() => {
+                    expect(pwdBox.getAttribute('class')).toContain('ng-valid');
                 });
             });
         });
     });
+
+    /******************************************************************************************************* */
+    // EMAIL
+
+    // emailTestCases.valid.forEach((testCase) => {
+    //     it('Signin form: EMAIL INPUT : ' + testCase + '   SHOULD BE : VALID', () => {
+    //         emailBox.clear().then(() => {
+    //             emailBox.sendKeys(testCase).then(() => {
+    //                 expect(emailBox.getAttribute('class')).toContain('ng-valid');
+    //             });
+    //         });
+    //     });
+    // });
+
+    // emailTestCases.invalid.forEach((testCase) => {
+    //     it('Signin form: EMAIL INPUT : ' + testCase + '   SHOULD BE : INVALID', () => {
+    //         emailBox.clear().then(() => {
+    //             emailBox.sendKeys(testCase).then(() => {
+    //                 expect(emailBox.getAttribute('class')).toContain('ng-invalid');
+    //             });
+    //         });
+    //     });
+    // });
+
+    // /******************************************************************************************************* */
+    // // PASSWORD
+
+    // pwdTestCases.valid.forEach((testCase) => {
+    //     it('Signin form: PASSWORD INPUT : ' + testCase + '   SHOULD BE : VALID', () => {
+    //         pwdBox.clear().then(() => {
+    //             pwdBox.sendKeys(testCase).then(() => {
+    //                 expect(pwdBox.getAttribute('class')).toContain('ng-valid');
+    //             });
+    //         });
+    //     });
+    // });
+
+
+    // pwdTestCases.invalid.forEach((testCase) => {
+    //     it('Signin form: PASSWORD INPUT : ' + testCase + '   SHOULD BE : INVALID', () => {
+    //         pwdBox.clear().then(() => {
+    //             pwdBox.sendKeys(testCase).then(() => {
+    //                 expect(pwdBox.getAttribute('class')).toContain('ng-invalid');
+    //             });
+    //         });
+    //     });
+    // });
+
+
 
 
 });
---
+++ e2e/src/signin/testCases-signin-withall.ts
@@ -0,0 +1,13 @@
+'use strict';
+export const testCases = {
+    valid: [
+        'aya.karmo95@gmail.com',
+        'a1@aya' // pwd
+    ]
+    ,
+    invalid: [
+        'aya.hossam@hotmail.com',
+        'aya',
+    ]
+};
+
