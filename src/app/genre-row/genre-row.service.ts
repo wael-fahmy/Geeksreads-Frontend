e535597616081bc7648f57cf6ca5829041ced787
yarasalem98 <yaramohamedsalem1@gmail.com>
2019-04-18 16:41:53 +0200

    Fixing merge conflicts (By Ahmed Salah)

--- src/app/genre-row/genre-row.service.ts
+++ src/app/genre-row/genre-row.service.ts
@@ -3,13 +3,6 @@ import { Injectable } from '@angular/core';
 import { Subject, Observable } from 'rxjs';
 import { Row } from './genre-row.model';
 
-<<<<<<< HEAD
-
-@Injectable({ providedIn: 'root' })
-||||||| merged common ancestors
-@Injectable({ providedIn: 'root' })
-=======
->>>>>>> 442ee150677d96cd935bd5942dd37c2c5ae7631d
 /**
  *
  * Injectable
@@ -19,84 +12,6 @@ import { Row } from './genre-row.model';
 @Injectable({ providedIn: 'root' })
 export class RowServices {
 
-<<<<<<< HEAD
-    /**
-     * Creates an instance of RowServices.
-     * @param {HttpClient} http
-     * @memberof RowServices
-     */
-    constructor( private http: HttpClient) {}
-
-    /**
-     *
-     * @private
-     * @type {Row}
-     * @memberof RowServices
-     */
-    private Row: Row;
-
-    /**
-     *
-     * @private
-     * @memberof RowServices
-     */
-    private rowUpdated = new Subject<Row>();
-
-
-    /**
-     * This function gets the row data from the backend database
-     *
-     * @memberof RowServices
-     */
-    get_row() {
-        this.http.get<{ message: string , Row: Row }>('http://localhost:3000/api/genres').subscribe((RowData) => {
-            this.Row = RowData.Row ;
-            this.rowUpdated.next(this.Row);
-
-        });
-    }
-    /*
-    get__books_by_genre(): Observable<any> {
-        const param = new HttpParams().set('genrename');
-        return this.http.get('http://localhost:3000/api/genres', {parameter: param});
-    } 
-    */
-    
-||||||| merged common ancestors
-    /**
-     *Creates an instance of RowServices.
-     * @param {HttpClient} http
-     * @memberof RowServices
-     */
-    constructor( private http: HttpClient) {}
-
-    /**
-     *
-     * @private
-     * @type {Row}
-     * @memberof RowServices
-     */
-    private Row: Row;
-
-    /**
-     *
-     * @private
-     * @memberof RowServices
-     */
-    private rowUpdated = new Subject<Row>();
-
-    /**
-     *This function gets the row info
-     * @memberof RowServices
-     */
-    get_row() {
-        this.http.get<{ message: string , Row: Row }>('http://localhost:3000/api/genres').subscribe((RowData) => {
-            this.Row = RowData.Row ;
-            this.rowUpdated.next(this.Row);
-
-        });
-    }
-=======
   /**
    * Creates an instance of RowServices.
    * @param {HttpClient} http
@@ -130,17 +45,7 @@ export class RowServices {
 
     });
   }
->>>>>>> 442ee150677d96cd935bd5942dd37c2c5ae7631d
 
-<<<<<<< HEAD
-  /**
-   * this function is responsible for updating info
-   * @returns
-   * @memberof RowServices
-   */
-||||||| merged common ancestors
-
-=======
 
   /**
    *
@@ -148,7 +53,6 @@ export class RowServices {
    * @returns
    * @memberof RowServices
    */
->>>>>>> 442ee150677d96cd935bd5942dd37c2c5ae7631d
   get_row_updated() {
     return this.rowUpdated.asObservable();
   }
