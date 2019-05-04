4d46af8af46b2978f16e09412aa3e4129b7a072b
Kanda <m.mufeed1996@gmail.com>
2019-05-04 19:05:41 +0200

    added details services

--- src/app/book-details/book-details.component.ts
+++ src/app/book-details/book-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { BookTitle_Service } from './book-details.service';
+import { BookDetails } from './book-details.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-book-details',
@@ -10,17 +13,35 @@ export class BookDetailsComponent implements OnInit {
    *  Panel open state boolean
    */
   public panelOpenState: boolean;
-  @Input() bdition: string;
-  @Input() bASIN: string;
-  @Input() btitle: string;
-  @Input() bpages: string;
-  @Input() bpublished: string;
-  constructor() { }
+  bookID: string;
+  bdition: string [] = [];
+  bASIN: string [] = [];
+  btitle: string [] = [];
+  bpages: string [] = [];
+  bpublished: string [] = [];
+  private Sub_profile: Subscription;
+  public book_details: BookDetails[] = [];
+  book_index = 0;
+  constructor(public booktitle_service: BookTitle_Service) { }
   ngOnInit() {
-    this.CutDate();
+    this.bookID = localStorage.getItem('bookID');
+    this.booktitle_service.get_book_Info(this.bookID);                            // to get the user info from the service
+    // tslint:disable-next-line:variable-name
+    this.Sub_profile = this.booktitle_service.get_book_Info_updated().subscribe((book_Information: BookDetails[]) => {
+      this.book_details = book_Information;
+      this.SetBookInfor();
+      this.CutDate();
+    });
+  }
+  SetBookInfor() {
+    this.bdition[this.book_index] = this.book_details[this.book_index].Publisher;
+    this.bASIN[this.book_index] = this.book_details[this.book_index].ISBN;
+    this.btitle[this.book_index] = this.book_details[this.book_index].Title;
+    this.bpages[this.book_index] = this.book_details[this.book_index].Pages.toString();
+    this.bpublished[this.book_index] = this.book_details[this.book_index].Published;
   }
   CutDate() {
-    const word = this.bpublished.split('T');
-    this.bpublished = word[0];
+    const word = this.bpublished[this.book_index].split('T');
+    this.bpublished[this.book_index] = word[0];
   }
 }
--- src/app/book-details/book-details.model.ts
+++ src/app/book-details/book-details.model.ts
@@ -1,64 +1,17 @@
-/**
- *
- * mmodule used to carru json
- * @export
- * @interface Bookinformation
- */
-export interface Bookinformation {
-    /**
-     *
-     * book eddition variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_edition: string;
-    /**
-     *
-     * published year variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_published: string;
-    /**
-     *
-     * SIN Variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_ASIN: string;
-    /**
-     *
-     * book language variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_language: string;
-    /**
-     *
-     * book orignal name variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_title: string;
-    /**
-     *
-     * book awards vairable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_awards: string;
-    /**
-     *
-     * book characters variable
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_characters: string;
-    /**
-     *
-     * variable to carry book id
-     * @type {string}
-     * @memberof Bookinformation
-     */
-    book_id: string;
+export interface BookDetails {
+    BookId: string;
+    Title: string;
+    Cover: string;
+    Description: string;
+    ReadStatus: string;
+    AuthorId: string;
+    BookRating;
+    AuthorName: string;
+    Genre: string;
+    ISBN: string;
+    Pages: number;
+    Published: string;
+    Publisher: string;
+    message: string;
+    success: boolean;
 }
